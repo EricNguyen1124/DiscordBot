d98a875e9324dd67e343d6798763c3926fd033d5
SharkBearGator-PC <47893402+SharkBearGator@users.noreply.github.com>
2020-02-17 00:01:54 -0600

    added reading from event file and embedded message for current

--- base.py
+++ base.py
@@ -14,21 +14,40 @@ async def on_message(message):
 
     if message.content.startswith('$hello') and message.author.id == 217067578533740544:
         await message.channel.send('Please type in a new event!')
-
         msg = await client.wait_for('message')
-        file = open('Events', 'a')
-        file.write(msg.content + "\n")
-        await message.channel.send('THANKS')
-        file.close()
+
+        if msg.author != client.user:
+            file = open('Events', 'a')
+            file.write(msg.content + "\n")
+            await message.channel.send('THANKS')
+            file.close()
+
         return
 
     if message.content.startswith('$current'):
+        count = 0
         file = open('Events', 'r')
-        fileLines = file.readlines()
-        await message.channel.send('~~Listing events~~')
-        for i in file:
-            list.append(i)
-        print(fileLines[4])
+        embed = discord.Embed(title="Listing Events", description="", color=0x00ff00)
 
+        for i in file:
+            count += 1
+            embed.add_field(name=str(count), value=i, inline=False)
+
+        await message.channel.send(embed=embed)
+        file.close()
+        return
+
+    if message.content.startswith('$edit'):
+        count = 0
+        file = open('Events', 'r')
+        embed = discord.Embed(title="Listing Events", description="", color=0x00ff00)
+
+        for i in file:
+            count += 1
+            embed.add_field(name=str(count), value=i, inline=False)
+
+        embed.add_field(name='Instructions:', value='Please reply with what event you would like to edit', inline=False)
+        await message.channel.send(embed=embed)
+        file.close()
 
 client.run('Njc2OTE3MTUwOTE2NjczNTg2.Xkb9IQ.P5ECTi1g0ihEe4ouIzCHIgMM9vw')
